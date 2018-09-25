87431ed661d14cd6f47ccc2c9ac34799a2d04dab
Bruce <iambruce@me.com>
2018-09-25 07:03:27 -0700

    RunContainer - Support starting background containers

---
+++ run_container.go
@@ -0,0 +1,43 @@
+package magicland
+
+import (
+	"io"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+)
+
+// RunningContainer is the result of a RunnableContainer
+// execution
+type RunningContainer struct {
+	RunnableContainer
+	Stdout io.ReadWriter
+	Stderr io.ReadWriter
+	Stdin  io.Writer
+}
+
+func (this RuntimeConfiguration) stageContainer(builtContainer container.ContainerCreateCreatedBody, dockerClient *client.Client) *RunnableContainer {
+	return &RunnableContainer{
+		StagedContainer:      builtContainer,
+		RuntimeConfiguration: this,
+		DockerClient:         dockerClient,
+	}
+}
+
+func runContainer(ctx context.Context, rc RunnableContainer) (RunningContainer, error) {
+	runningContainer := RunningContainer{
+		RunnableContainer: rc,
+	}
+	err := rc.DockerClient.ContainerStart(
+		ctx,
+		rc.StagedContainer.ID,
+		types.ContainerStartOptions{},
+	)
+	if err != nil {
+		return runningContainer, err
+	}
+
+	return runningContainer, nil
+}
